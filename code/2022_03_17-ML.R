e70b4605632458b46c0f9e37ed621089a0eafa9d
Ashley Naimi <ashley.naimi@emory.edu>
2022-03-30 09:37:35 -0400

    created playground to get CV SL with variable importance to work

--- code/2022_03_17-ML.R
+++ code/2022_03_17-ML.R
@@ -64,25 +64,39 @@ task <- sl3_Task$new(
   outcome = "ptb37"
   )
 
+task_CV <- sl3_Task$new(
+  data = train, ## should this be fit to entire dataset?
+  covariates = covars,
+  outcome = "ptb37",
+  folds = origami::make_folds(train, fold_fun = folds_vfold, V = 3)
+  )
+
 # set up learners via built-in functions and pipelines
 glm_learner <- Lrnr_glm$new()
 SL.glmnet_learner <- Lrnr_pkg_SuperLearner$new(SL_wrapper = "SL.glmnet")
 SL.ranger <-Lrnr_ranger$new()
 SL.mean <- Lrnr_mean$new()
 
-# stack learners into a model (including screeners and pipelines)
 learner_stack <- Stack$new(SL.glmnet_learner, glm_learner, SL.ranger, SL.mean)
-stack_fit <- learner_stack$train(task)
-preds <- stack_fit$predict()
-head(preds)
 
-#TO DOs
-#1 - Cross validation settings
+sl <- Lrnr_sl$new(learner_stack) ## check to see if this allows us to use CV SL?
+sl_fit <- sl$train(task)
+
+## this is not working, something wrong with the loss function...
+CVsl <- CV_lrnr_sl(
+  lrnr_sl = sl_fit,
+  task = task_CV,
+  loss_fun = loss_loglik_binomial
+)
+
+
+hei_varimp <- importance(sl_fit, loss = loss_squared_error, type = "permute")
+
 
 cv_stack <- Lrnr_cv$new(learner_stack)
 cv_fit <- cv_stack$train(task)
 cv_preds <- cv_fit$predict()
-risks <- cv_fit$cv_risk(loss_squared_error)
+risks <- cv_fit$cv_risk(loss_loglik_binomial)
 print(risks)
 head(cv_preds)
 dim(cv_preds)
@@ -92,4 +106,56 @@ dim(cv_preds)
 #4 - Select loss function and performance metrics
 
 
+#### playground ####
 
+db_data <- url(
+  "https://raw.githubusercontent.com/benkeser/sllecture/master/chspred.csv"
+)
+chspred <- read_csv(file = db_data, col_names = TRUE)
+data.table::setDT(chspred)
+
+# make task
+chspred_task <- make_sl3_Task(
+  data = chspred,
+  covariates = colnames(chspred)[-1],
+  outcome = "mi"
+)
+
+# make learners
+glm_learner <- Lrnr_glm$new()
+lasso_learner <- Lrnr_glmnet$new(alpha = 1)
+ridge_learner <- Lrnr_glmnet$new(alpha = 0)
+enet_learner <- Lrnr_glmnet$new(alpha = 0.5)
+# curated_glm_learner uses formula = "mi ~ smoke + beta"
+curated_glm_learner <- Lrnr_glm_fast$new(covariates = c("smoke", "beta"))
+mean_learner <- Lrnr_mean$new() # That is one mean learner!
+glm_fast_learner <- Lrnr_glm_fast$new()
+ranger_learner <- Lrnr_ranger$new()
+svm_learner <- Lrnr_svm$new()
+xgb_learner <- Lrnr_xgboost$new()
+
+# screening
+screen_cor <- make_learner(Lrnr_screener_correlation)
+glm_pipeline <- make_learner(Pipeline, screen_cor, glm_learner)
+
+# stack learners together
+stack <- make_learner(
+  Stack,
+  glm_pipeline, glm_learner,
+  lasso_learner, ridge_learner, enet_learner,
+  curated_glm_learner, mean_learner, glm_fast_learner,
+  ranger_learner, svm_learner, xgb_learner
+)
+
+# make and train SL
+sl <- Lrnr_sl$new(
+  learners = stack
+)
+sl_fit <- sl$train(chspred_task)
+sl_fit$cv_risk(loss_squared_error)
+
+CVsl <- CV_lrnr_sl(sl_fit, chspred_task, loss_squared_error)
+CVsl
+
+varimp <- importance(sl_fit)
+importance_plot(varimp) 
