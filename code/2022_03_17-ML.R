da5f4868748426398d04e8c5cfc4924004af74c7
Ashley Naimi <ashley.naimi@emory.edu>
2022-04-06 09:48:13 -0400

    MAJOR CHANGE: CV SL, updated predictions, variable imp function, and figures for pred v obs, auc, and var imp figure

--- code/2022_03_17-ML.R
+++ code/2022_03_17-ML.R
@@ -1,3 +1,4 @@
+## Ashley's code for install ##
 packages <- c("data.table","tidyverse","skimr","here","remotes",
               "SuperLearner","glmnet","ranger","origami","caret")
 
@@ -22,12 +23,50 @@ thm <- theme_classic() +
   )
 theme_set(thm)
 
-## WARNING! THIS NEEDS TO BE CHANGED
+## Julie's code for install ##
+install.packages ("remotes")
+install.packages ("SuperLearner")
+install.packages(c("caret", "glmnet", "ggplot2", "RhpcBLASctl", "ranger"))
+install.packages("randomForest")
+install.packages("tidyverse")
+install.packages("origami")
+remotes::install_github("tlverse/sl3")
+install.packages ("data.table")
+install.packages ("dplyr")
+install.packages ("readr")
+install.packages ("knitr")
+install.packages ("kableExtra")
+install.packages ("ROCR")
+install.packages("varImp")
+install.packages("purrr")  
+install.packages("caret")
+install.packages("Rsolnp")
+
+set.seed(49753)
+library(tidyverse)
+library(data.table)
+library(SuperLearner)
+library(origami)
+library(sl3)
+library(data.table)
+library(dplyr)
+library(readr)
+library(knitr)
+library(kableExtra)
+library(ROCR) # for AUC calculation
+library(varImp)
+library(purrr)
+library(caret)
+library(Rsolnp)
+
+## Reading in data ##
+## This will need changing: we should use the "here()" function
 if(Sys.info()["nodename"]=="EPIC02G44CTQ05P"|Sys.info()["nodename"]=="EPI9TPH7M3"){
   a <- read_csv(here("data","numom750.csv"))
   a_scaled <- read_csv(here("data","2022_03_18-scaled_hei.csv"))
 } else{
-  mydata <- read.csv("I:\\Bodnar Julie\\numom750.csv")  
+  a <- read.csv("C:\\Diet R01\\HEI Super Learner\\numom750.csv") 
+  a_scaled <- read_csv(here("C:\\Diet R01\\HEI Super Learner\\2022_04_05-scaled_hei.csv"))
 }
 
 names(a)
@@ -38,38 +77,45 @@ sl3_list_learners(c("binomial"))
 covars <- names(a_scaled)
 mydata <- cbind(tibble(ptb37 = a$ptb37),a_scaled)
 
-head(mydata)
+names(mydata)
+
 
 # create training and testing data
+## This step is not needed because SL will automatically create folds for 
+## training and validation
 set.seed(123)
-y = factor(mydata$ptb37)
+#y = factor(mydata$ptb37)
 
-index <- createDataPartition(
-  y,
-  times = 1,
-  p = .7,
-  list = TRUE,
-  groups = min(5, length(y))
-)
+#index <- createDataPartition(
+#  y,
+#  times = 1,
+#  p = .7,
+#  list = TRUE,
+#  groups = min(5, length(y))
+#)
 
-train <- mydata[index$Resample1,]
-test <- mydata[-index$Resample1,]
+#train <- mydata[index$Resample1,]
+#test <- mydata[-index$Resample1,]
 
-nrow(test)
-nrow(train)
+#nrow(train)
+#nrow(test)
 
-task <- sl3_Task$new(
-  data = train,
-  covariates = covars,
-  outcome = "ptb37"
-  )
-
-task_CV <- sl3_Task$new(
-  data = train, ## should this be fit to entire dataset?
+#Defining ML task - sl3_task object keeps track of the roles the variables 
+#play in the machine learning problem, the data, and any metadata
+#uses cross-validation (CV) to build the ensemble model
+task <- make_sl3_Task(
+  data = mydata,
   covariates = covars,
   outcome = "ptb37",
-  folds = origami::make_folds(train, fold_fun = folds_vfold, V = 3)
-  )
+  folds = 10L
+)
+
+task
+
+length(task$folds) # how many folds? 10 by default
+
+head(task$folds[[1]]$training_set)
+head(task$folds[[1]]$validation_set)
 
 # set up learners via built-in functions and pipelines
 glm_learner <- Lrnr_glm$new()
@@ -77,85 +123,82 @@ SL.glmnet_learner <- Lrnr_pkg_SuperLearner$new(SL_wrapper = "SL.glmnet")
 SL.ranger <-Lrnr_ranger$new()
 SL.mean <- Lrnr_mean$new()
 
-learner_stack <- Stack$new(SL.glmnet_learner, glm_learner, SL.ranger, SL.mean)
+# named vector of learners first
+learners <- c(
+  glm_learner, SL.glmnet_learner, SL.ranger, SL.mean
+)
+names(learners) <- c(
+  "glm", "glm net", "ranger", "mean"
+)
 
-sl <- Lrnr_sl$new(learner_stack) ## check to see if this allows us to use CV SL?
+learner_stack <- make_learner(Stack, learners)
+learner_stack
+
+#Lrnr_sl object specifies the Super Learner
+sl <- Lrnr_sl$new(learner_stack)
+sl
+
+#The SL algorithm fits a metalearner on the validation-set predictions 
+#in a cross-validated manner, thereby avoiding overfitting.
 sl_fit <- sl$train(task)
+sl_fit
 
-## this is not working, something wrong with the loss function...
-CVsl <- CV_lrnr_sl(
-  lrnr_sl = sl_fit,
-  task = task_CV,
-  loss_fun = loss_loglik_binomial
-)
-
-
-hei_varimp <- importance(sl_fit, loss = loss_squared_error, type = "permute")
-
-
-cv_stack <- Lrnr_cv$new(learner_stack)
-cv_fit <- cv_stack$train(task)
-cv_preds <- cv_fit$predict()
-risks <- cv_fit$cv_risk(loss_loglik_binomial)
+risks <- sl_fit$cv_risk(loss_loglik_binomial)
 print(risks)
-head(cv_preds)
-dim(cv_preds)
 
-#2 - Permutation based variable importance
-#3 - Create training and validation data set
-#4 - Select loss function and performance metrics
+#SL predictions
+sl_preds <- sl_fit$predict()
+head(sl_preds)
+summary(sl_preds)
 
+qplot(mydata$ptb37, sl_preds, main = 
+        # "lrn_mean"
+        # "lrn_glm"
+        # "lrn_ranger100"
+        "SuperLearner"
+) + theme_minimal()
 
-#### playground ####
-
-db_data <- url(
-  "https://raw.githubusercontent.com/benkeser/sllecture/master/chspred.csv"
+AUCsl <- performance(prediction(sl_preds, mydata$ptb37), measure = "auc")@y.values[[1]]
+plot(performance(prediction(sl_preds, mydata$ptb37), "tpr", "fpr"), main = 
+       # "lrn_mean"
+       # "lrn_glm"
+       # "lrn_ranger100"
+       "SuperLearner" 
 )
-chspred <- read_csv(file = db_data, col_names = TRUE)
-data.table::setDT(chspred)
+AUCsl
 
-# make task
-chspred_task <- make_sl3_Task(
-  data = chspred,
-  covariates = colnames(chspred)[-1],
-  outcome = "mi"
-)
-
-# make learners
-glm_learner <- Lrnr_glm$new()
-lasso_learner <- Lrnr_glmnet$new(alpha = 1)
-ridge_learner <- Lrnr_glmnet$new(alpha = 0)
-enet_learner <- Lrnr_glmnet$new(alpha = 0.5)
-# curated_glm_learner uses formula = "mi ~ smoke + beta"
-curated_glm_learner <- Lrnr_glm_fast$new(covariates = c("smoke", "beta"))
-mean_learner <- Lrnr_mean$new() # That is one mean learner!
-glm_fast_learner <- Lrnr_glm_fast$new()
-ranger_learner <- Lrnr_ranger$new()
-svm_learner <- Lrnr_svm$new()
-xgb_learner <- Lrnr_xgboost$new()
-
-# screening
-screen_cor <- make_learner(Lrnr_screener_correlation)
-glm_pipeline <- make_learner(Pipeline, screen_cor, glm_learner)
-
-# stack learners together
-stack <- make_learner(
-  Stack,
-  glm_pipeline, glm_learner,
-  lasso_learner, ridge_learner, enet_learner,
-  curated_glm_learner, mean_learner, glm_fast_learner,
-  ranger_learner, svm_learner, xgb_learner
+# Cross validation using SL3 - if not specified 10 folds is default
+#We can cross-validate the SL to see how well the SL performs on unseen data, 
+#and obtain an estimate of the cross-validated risk of the SL
+task_CVsl <- make_sl3_Task(
+  data = mydata,
+  outcome = "ptb37",
+  covariates = covars,
+  drop_missing_outcome = TRUE,
+  folds = origami::make_folds(
+    n = sum(!is.na(mydata$ptb37)),
+    fold_fun = folds_vfold,
+    V = 10
+  )
 )
 
-# make and train SL
-sl <- Lrnr_sl$new(
-  learners = stack
-)
-sl_fit <- sl$train(chspred_task)
-sl_fit$cv_risk(loss_squared_error)
+#Not sure if below code is needed
+#cv_stack <- Lrnr_cv$new(learner_stack)
+#cv_stack
 
-CVsl <- CV_lrnr_sl(sl_fit, chspred_task, loss_squared_error)
+CVsl <- CV_lrnr_sl(sl_fit, task_CVsl, loss_loglik_binomial)
 CVsl
 
-varimp <- importance(sl_fit)
-importance_plot(varimp) 
+# Permutation based variable importance plot
+
+# sl3 variable importance plot
+## why do we use sl_fit here, and not an output of the second CV run?
+varimp <- importance(sl_fit, loss_loglik_binomial, type = "permute")
+
+#importance automatically set based on outcome type
+#for constant and binomial outcomes set to loss_squared_error
+#see https://github.com/tlverse/sl3/blob/master/R/importance.R
+    
+dotchart(labels = varimp$covariate, varimp$NLL_difference, xlab="Variable Importance", col='darkblue', pch=16, cex=1.1)
+abline(v=abs(min(varimp$NLL_difference)), col='red', lty='longdash', lwd=2)
+
